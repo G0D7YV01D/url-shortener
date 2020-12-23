fe39f47d73757e02141317e4e18ad0bc3beaffcf
venomhaxor <68974601+venomhaxor@users.noreply.github.com>
2020-12-22 23:41:20 -0600

    Update short.sh

--- short.sh
+++ short.sh
@@ -1,7 +1,3 @@
-# url-shortener
-Url shortner
-# Bash Script for Hide URL FIXED by VENXM
-
 url_checker() {
     if [ ! "${1//:*}" = http ]; then
         if [ ! "${1//:*}" = https ]; then
@@ -11,12 +7,24 @@ url_checker() {
     fi
 }
 
-echo -n "Paste URL here (with http or https): "
-read URL
-url_checker $url
+echo -e "\n\e[1;31;42m######┌──────────────────────────┐##### \e[0m"
+echo -e "\e[1;31;42m######│▙▗▌      ▌  ▛▀▖▌  ▗    ▌  │##### \e[0m"
+echo -e "\e[1;31;42m######│▌▘▌▝▀▖▞▀▘▌▗▘▙▄▘▛▀▖▄ ▞▀▘▛▀▖│##### \e[0m"
+echo -e "\e[1;31;42m######│▌ ▌▞▀▌▝▀▖▛▚ ▌  ▌ ▌▐ ▝▀▖▌ ▌│##### \e[0m"
+echo -e "\e[1;31;42m######│▘ ▘▝▀▘▀▀ ▘ ▘▘  ▘ ▘▀▘▀▀ ▘ ▘│##### \e[0m"
+echo -e "\e[1;31;42m######└──────────────────────────┘##### \e[0m \n"
+echo -e "\e[40;38;5;82m Please Visit \e[30;48;5;82m https://www.kalilinux.in \e[0m"
+echo -e "\e[30;48;5;82m    Copyright \e[40;38;5;82m   JayKali \e[0m \n\n"
+echo -e "\e[1;31;42m ### Phishing URL ###\e[0m \n"
+echo -n "Paste Phishing URL here (with http or https): "
+read phish
+url_checker $phish
+sleep 1
+echo "Processing and Modifing Phishing URL"
+echo ""
 shorter=${short#https://}
 echo -e "\n\e[1;31;42m ### Masking Domain ###\e[0m"
-echo 'Domain to mask the URL (with http or https), ex https://google.com, http
+echo 'Domain to mask the Phishing URL (with http or https), ex: https://google.com, http
 ://anything.org) :'
 echo -en "\e[32m=>\e[0m "
 read mask
@@ -26,5 +34,5 @@ echo -e "\e[31mDon't use space just use '-' between social engineering words\e[0
 echo -en "\e[32m=>\e[0m "
 read words
 echo -e "\nGenerating MaskPhish Link...\n"
-final=$mask$words
-echo -e "Here is the URL:\e[32m ${final} \e[0m\n"
+final=$mask-$words
+echo -e "Here is the MaskPhish URL:\e[32m ${final} \e[0m\n"
