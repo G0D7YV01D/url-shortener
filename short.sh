564d6222640cb6da6a3e01ee563f7e72ef14e932
venomhaxor <68974601+venomhaxor@users.noreply.github.com>
2020-12-22 19:35:38 -0600

    Update short.sh

--- short.sh
+++ short.sh
@@ -1,6 +1,6 @@
 # url-shortener
 Url shortner
-# Bash Script for Hide Phishing URL Created by KP
+# Bash Script for Hide URL FIXED by VENXM
 
 url_checker() {
     if [ ! "${1//:*}" = http ]; then
@@ -11,12 +11,12 @@ url_checker() {
     fi
 }
 
-echo -n "Paste Phishing URL here (with http or https): "
-read phish
-url_checker $phish
+echo -n "Paste URL here (with http or https): "
+read URL
+url_checker $url
 shorter=${short#https://}
 echo -e "\n\e[1;31;42m ### Masking Domain ###\e[0m"
-echo 'Domain to mask the Phishing URL (with http or https), ex https://google.com, http
+echo 'Domain to mask the URL (with http or https), ex https://google.com, http
 ://anything.org) :'
 echo -en "\e[32m=>\e[0m "
 read mask
@@ -27,4 +27,4 @@ echo -en "\e[32m=>\e[0m "
 read words
 echo -e "\nGenerating MaskPhish Link...\n"
 final=$mask$words
-echo -e "Here is the MaskPhish URL:\e[32m ${final} \e[0m\n"
+echo -e "Here is the URL:\e[32m ${final} \e[0m\n"
