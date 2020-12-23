4e35f5aae1b4d7f7b5730bd808ae8c653452c806
venomhaxor <68974601+venomhaxor@users.noreply.github.com>
2020-12-22 23:51:14 -0600

    Update short.sh

--- short.sh
+++ short.sh
@@ -15,16 +15,16 @@ echo -e "\e[1;31;42m######│▘ ▘▝▀▘▀▀ ▘ ▘▘  ▘ ▘▀▘▀
 echo -e "\e[1;31;42m######└──────────────────────────┘##### \e[0m \n"
 echo -e "\e[40;38;5;82m Please Visit \e[30;48;5;82m https://www.kalilinux.in \e[0m"
 echo -e "\e[30;48;5;82m    Copyright \e[40;38;5;82m   JayKali \e[0m \n\n"
-echo -e "\e[1;31;42m ### Phishing URL ###\e[0m \n"
-echo -n "Paste Phishing URL here (with http or https): "
-read phish
-url_checker $phish
+echo -e "\e[1;31;42m ### URL ###\e[0m \n"
+echo -n "Paste URL here (with http or https): "
+read url
+url_checker $url
 sleep 1
-echo "Processing and Modifing Phishing URL"
+echo "Processing and Modifing URL"
 echo ""
 shorter=${short#https://}
 echo -e "\n\e[1;31;42m ### Masking Domain ###\e[0m"
-echo 'Domain to mask the Phishing URL (with http or https), ex: https://google.com, http
+echo 'Domain to mask the URL (with http or https), ex: https://google.com, http
 ://anything.org) :'
 echo -en "\e[32m=>\e[0m "
 read mask
@@ -34,5 +34,5 @@ echo -e "\e[31mDon't use space just use '-' between social engineering words\e[0
 echo -en "\e[32m=>\e[0m "
 read words
 echo -e "\nGenerating MaskPhish Link...\n"
-final=$mask-$words
+final=$mask$words
 echo -e "Here is the MaskPhish URL:\e[32m ${final} \e[0m\n"
